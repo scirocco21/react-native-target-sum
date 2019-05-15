7033c8ff6e7a6bab09f596f36d2c9add20884e47
Sebastian <sebgertz@gmail.com>
2019-05-15 17:20:33 -0400

    generate target number from random addables and add styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,12 +5,20 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 
 export default class Game extends React.Component {
   render() {
-    const target = Math.floor(Math.random() * 40) + 10;
-    // <Text>{this.props.randomNumbers}</Text>
+
+    let addableNumbers = Array.from({length: this.props.randomNumbers}).map(() => Math.floor(Math.random() * 10));
+    let target = addableNumbers.slice(0, 4).reduce((sum, num) => sum += num)
+
+    let addables = addableNumbers.map(num => {
+      return (
+        <Text style={styles.addable}>{num}</Text>
+      )
+    })
+
     return (
       <View style={styles.container}>
         <Text style={styles.target}>{target}</Text>
-
+        <View style={styles.numbersContainer}>{addables}</View>
       </View>
     );
   }
@@ -25,8 +33,21 @@ const styles = StyleSheet.create({
   target: {
     fontSize: 60,
     textAlign: 'center',
-    margin: 10,
+    margin: 15,
     padding: 20,
     backgroundColor: '#FFE9B2',
   },
+  numbersContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginTop: 100,
+  },
+  addable: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 25,
+    padding: 20,
+    backgroundColor: '#FF977E',
+  }
 });
